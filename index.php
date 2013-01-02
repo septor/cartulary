fc5116d2c4df5c06f7002b1c7158075df3f3bbbd
septor <patrickweaver@gmail.com>
2013-01-02 08:55:48 -0500

    make some of the XML fields required

--- index.php
+++ index.php
@@ -48,47 +48,50 @@ if(!file_exists("data/".$config['cache']) || time() - filemtime("data/".$config[
 					$color = $category['color'];
 					foreach($category->download as $download)
 					{
-						foreach($gi as $data)
+						if(!empty($download['name']) && !empty($download['info']) && !empty($download['github']) && !empty($download['file']))
 						{
-							if(substr($data, 0, strlen(trim($download['github']))) == trim($download['github']))
+							foreach($gi as $data)
 							{
-								$datas = explode("__", $data);
+								if(substr($data, 0, strlen(trim($download['github']))) == trim($download['github']))
+								{
+									$datas = explode("__", $data);
+								}
 							}
+							$repo_url = "https://github.com/".$config['github']."/".$download['github'];
+
+							echo "
+							<div class='download-block'>
+								<div class='category' style='background-color: #".$color.";'></div>
+								<b>".$download['name']."</b>
+								<span class='commitverlog'>
+									".(!empty($download['version']) ? "v".$download['version']."<br />" : "")."
+									<a href='log.php?repo=".$download['github']."'>".INDEX_LAN03."</a>, <a href='".$repo_url."/issues'>".INDEX_LAN04."</a>
+								</span>
+								<br />
+								<span>".$download['info']."</span>
+								<br /><br />
+								<span class='button' style='float:left;'><a href='".$repo_url."'>".INDEX_LAN05."</a></span>
+								<span class='count'><a href='".$repo_url."/network'>".number_format($datas[1])."</a></span>
+
+								<span class='button' style='float:right;'><a href='".$repo_url."'>".INDEX_LAN06."</a></span>
+								<span class='rcount' style='float:right;'><a href='".$repo_url."/stargazers'>".number_format($datas[2])."</a></span>
+
+								<div style='height: 10px;'></div>
+
+								<span class='button' style='float:left;'><a href='get.php?file=".$download['github']."'>".INDEX_LAN07."</a></span>
+								<span class='count'>".number_format(get_count($download['github'], "get"))."</span>";
+
+								if(!empty($download['adfly']))
+								{
+									echo "
+									<span class='button' style='float:right;'><a href='http://adf.ly/".$download['adfly']."'>Ad.Fly</a></span>
+									<span class='rcount' style='float:right;'>".number_format(get_count($download['github'], "tip"))."</span>";
+								}
+
+							echo "
+							</div>
+							";
 						}
-						$repo_url = "https://github.com/".$config['github']."/".$download['github'];
-
-						echo "
-						<div class='download-block'>
-							<div class='category' style='background-color: #".$color.";'></div>
-							<b>".$download['name']."</b>
-							<span class='commitverlog'>
-								".(!empty($download['version']) ? "v".$download['version']."<br />" : "")."
-								<a href='log.php?repo=".$download['github']."'>".INDEX_LAN03."</a>, <a href='".$repo_url."/issues'>".INDEX_LAN04."</a>
-							</span>
-							<br />
-							<span>".$download['info']."</span>
-							<br /><br />
-							<span class='button' style='float:left;'><a href='".$repo_url."'>".INDEX_LAN05."</a></span>
-							<span class='count'><a href='".$repo_url."/network'>".number_format($datas[1])."</a></span>
-
-							<span class='button' style='float:right;'><a href='".$repo_url."'>".INDEX_LAN06."</a></span>
-							<span class='rcount' style='float:right;'><a href='".$repo_url."/stargazers'>".number_format($datas[2])."</a></span>
-
-							<div style='height: 10px;'></div>
-
-							<span class='button' style='float:left;'><a href='get.php?file=".$download['github']."'>".INDEX_LAN07."</a></span>
-							<span class='count'>".number_format(get_count($download['github'], "get"))."</span>";
-
-							if(!empty($download['adfly']))
-							{
-								echo "
-								<span class='button' style='float:right;'><a href='http://adf.ly/".$download['adfly']."'>Ad.Fly</a></span>
-								<span class='rcount' style='float:right;'>".number_format(get_count($download['github'], "tip"))."</span>";
-							}
-
-						echo "
-						</div>
-						";
 					}
 				}
 
