d64ab3294d6b33916fde68298b821a509bf50048
septor <patrickweaver@gmail.com>
2013-01-03 10:23:27 -0500

    let's put the donate link on the log page

--- log.php
+++ log.php
@@ -26,6 +26,7 @@ $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
 			<div id='bar'>
 				<b><?php echo $config['sitename']; ?></b><br />
 				<div class='tag'><?php echo $config['sitetag']; ?></div>
+				<?php if(!empty($config['paypal'])) { ?> <div class='donate'><a href='<?php echo $config['paypal']; ?>'><?php echo INDEX_LAN08; ?></a></div> <?php } ?>
 			</div>
 
 			<div id="content">
